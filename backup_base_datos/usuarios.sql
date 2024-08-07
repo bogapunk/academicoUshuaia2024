cd5bb2edf6b597ac35ad688bae57324ed01cad44
gabi <gabi@debian.gabi>
2024-08-07 08:15:26 -0300

    Deploy base de datos

--- backup_base_datos/usuarios.sql
+++ backup_base_datos/usuarios.sql
@@ -1,63 +1,39 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 07-03-2018 a las 16:14:20
--- Versión del servidor: 5.6.21
--- Versión de PHP: 5.6.3
+-- Configuración inicial (No es necesario en SQL Server)
+-- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- SET time_zone = "+00:00";
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+-- No son necesarias las configuraciones de MySQL en SQL Server
+-- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+-- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+-- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- /*!40101 SET NAMES utf8 */;
+
+-- Levanto base de datos de backup
+USE master;
+GO
+CREATE DATABASE junta
+ON (FILENAME= '/var/opt/mssql/data/junta.mdf'),(FILENAME= '/var/opt/mssql/data/junta.ldf') FOR ATTACH;
+GO
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+-- Usar la base de datos
+USE junta;
+GO
 
---
--- Base de datos: `php_usuarios`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
---
-
-CREATE TABLE IF NOT EXISTS `usuarios` (
-`id` int(11) NOT NULL,
-  `nombres` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `apellidos` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `email` varchar(150) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `password` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `telefono` varchar(15) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `olvido_pass_iden` varchar(32) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `creado` datetime DEFAULT NULL,
-  `modificado` datetime DEFAULT NULL,
-  `estado` enum('1','0') COLLATE utf8_unicode_ci DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `usuarios`
---
-ALTER TABLE `usuarios`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- Crear la tabla `usuarios`
+IF OBJECT_ID('usuarios', 'U') IS NULL
+BEGIN
+CREATE TABLE usuarios (
+                          id INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+                          nombres NVARCHAR(150) NULL,
+                          apellidos NVARCHAR(150) NULL,
+                          email NVARCHAR(150) NULL,
+                          password NVARCHAR(120) NULL,
+                          telefono NVARCHAR(15) NULL,
+                          olvido_pass_iden NVARCHAR(32) NULL,
+                          creado DATETIME NULL,
+                          modificado DATETIME NULL,
+                          estado NVARCHAR(1) DEFAULT '1'
+);
+END
+GO
