77fca3176eb5a754eba3dec08377bc82112fae3c
bogapunk <ing.horacio.bogarin@gmail.com>
2024-12-06 11:28:50 -0300

    modificacion de ruta pdf docentes

--- junta/views/Docentes/generate_pdf.php
+++ junta/views/Docentes/generate_pdf.php
@@ -144,5 +144,6 @@ EOD;
 $pdf->writeHTML($html, true, false, true, false, '');
 
 // Enviar el PDF al navegador como descarga
-$pdf->Output('docente_info.pdf', 'D'); // 'D' para descargar
+$pdf->Output('docente_info.pdf', '/home/hbogarin/academicoUshuaia2024/junta/pdf
+'); // 'server juntas 2024' para descargar
 ?>
