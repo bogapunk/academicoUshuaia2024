163bc10a6961bc6f40dbdf364b8688d0268a9d16
bogapunk <ing.horacio.bogarin@gmail.com>
2026-02-18 15:06:15 -0300

    se el listado de provinciales donde se agrego localidad porvincial para rio grande - ushuaia  y tolhuin 2026

--- junta/views/ListadoDeDocentes/ListarListadosDeDocentes.php
+++ junta/views/ListadoDeDocentes/ListarListadosDeDocentes.php
@@ -550,7 +550,7 @@ $(document).ready(function() {
 				  <option value="RGD">Río Grande</option>
 				  <option value="TOL">Tolhuin</option>
 				  <option value="ANT">Antártida</option>
-          <option value="PROVIN">PROVINCIALES</option>
+          <option value="PROVIN">Provincial</option>
 				</select>
  
  	</td>
